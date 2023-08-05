3fe76d8be6f913fc96f9f62bf32722d4edd54511
Mustafa <mostafa.dash34@gmail.com>
2023-08-06 00:33:55 +0300

    modified:   src/controllers/auth.controller.ts

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -5,16 +5,18 @@ import User from '../models/User.js';
 import config from '../config/index.js';
 import NotFoundError from '../errors/not-found.js';
 import { asyncWrapper } from '../middlewares/async.middleware.js';
+import { CustomError, UnauthenticatedError } from '../errors/index.js';
+import { StatusCodes } from 'http-status-codes';
 
 export const signup = asyncWrapper(async (req: Request, res: Response) => {
   const { username, password, age, email } = req.body;
-  const userExists = await User.find({
-    $or: [{ Username: username }, { Email: email }],
-  });
-  console.log(userExists);
-
-  if (userExists.length > 0) {
-    return res.json({ Errors: 'User Already Exists' });
+  const usernameExists = await User.findOne({ Username: username });
+  if (usernameExists) {
+    throw new CustomError('Username already exists', StatusCodes.BAD_REQUEST);
+  }
+  const emailExists = await User.findOne({ Email: email });
+  if (emailExists) {
+    throw new CustomError('Email already exists', StatusCodes.BAD_REQUEST);
   }
   const newUser = new User({
     Username: username,
@@ -24,7 +26,8 @@ export const signup = asyncWrapper(async (req: Request, res: Response) => {
   });
   console.log(newUser);
   await newUser.save();
-  const { Password, ...user } = newUser.toObject();
+  const { Password, WatchLaterMovies, WatchedMovies, FavoriteMovies, ...user } =
+    newUser.toObject();
   const token = jwt.sign(
     {
       _id: user._id,
@@ -39,13 +42,15 @@ export const signup = asyncWrapper(async (req: Request, res: Response) => {
 
 export const login = asyncWrapper(async (req: Request, res: Response) => {
   const { username, password } = req.body;
-  const User_response = await User.findOne({ Username: username });
+  const User_response = await User.findOne({
+    $or: [{ Username: username }, { Email: username }],
+  });
   if (!User_response) {
     throw new NotFoundError('User Not Found');
   }
   const isMatch = bcryptjs.compareSync(password, User_response.Password);
   if (!isMatch) {
-    throw new Error('Invalid Password');
+    throw new UnauthenticatedError('Password Incorrect');
   }
   const { Password, WatchedMovies, FavoriteMovies, WatchLaterMovies, ...user } =
     User_response.toObject();
